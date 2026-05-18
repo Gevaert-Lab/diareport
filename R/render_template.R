cbb71ada04cb1709a40e3b12b75573bc9cc8ce25
Andrea Argentini <a.argentini@gmail.com>
2026-05-18 14:39:55 +0200

    Add new median-based normalization methods
    - Introduce 'medianOfRatios' and 'logMedian' normalization options.
    - Update parameter validation to include these new methods.
    - Implementation is a bit redundant / TO DO : remove duplicated code
    - Implement normalization logic using sweep functions from msqrob2.
    - Refactor normalization flow for improved consistency.
    - Correct a minor typo in the report template.

--- R/render_template.R
+++ R/render_template.R
@@ -316,8 +316,8 @@ validate_params <- function(params) {
     ),
     normalization = list(
       type = "string",
-      check = function(x) x %in%  c("sum", "max", "center.mean", "center.median", "div.mean", "div.median", "diff.median", "quantiles","quantiles.robust","vsn"),
-      msg = "Normalization must be a recognized method among: [sum max center.mean center.median div.mean div.median diff.median quantiles quantiles.robust vsn]"
+      check = function(x) x %in%  c("sum", "max", "center.mean", "center.median", "div.mean", "div.median", "diff.median", "quantiles","quantiles.robust","vsn","medianOfRatios",'logMedian'),
+      msg = "Normalization must be a recognized method among: [sum max center.mean center.median div.mean div.median diff.median quantiles quantiles.robust vsn medianOfRatios logMedian]"
     ),
     FC_thr = list(
       type = "numeric",
